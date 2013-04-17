13ccc5b25e50714a1413d67075184c3c2d0bb3b3
cyrusin <lysh0526@gmail.com>
2013-04-17 17:37:49 +0800

    simple version of url fetch

--- url_fetch.py
+++ url_fetch.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
 #@Author: cyrusin
+import threading
+import time
+import urllib2
 
+hosts = ["http://yahoo.com", \
+            "http://google.com", \
+            "http://amazon.com", \
+            "http://ibm.com", \
+            "http://apple.com"]
+start = time.time()
 
+for host in hosts:
+    url = urllib2.urlopen(host)
+    print url.read(1024)
+
+print "Elapsed Time: %s" % (time.time() - start)
