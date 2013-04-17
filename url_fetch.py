3da4d450f0dbce4139fe9263aa381b1106001604
cyrusin <lysh0526@gmail.com>
2013-04-17 18:05:51 +0800

    add threading to url_fetch.py

--- url_fetch.py
+++ url_fetch.py
@@ -4,16 +4,40 @@
 import threading
 import time
 import urllib2
+import Queue
 
 hosts = ["http://yahoo.com", \
             "http://google.com", \
             "http://amazon.com", \
             "http://ibm.com", \
             "http://apple.com"]
+
+queue = Queue.Queue()
+class ThreadUrl(threading.Thread):
+    """class ThreadUrl(threading.Thread)
+
+    """
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            host = self.queue.get()
+            url = urllib2.urlopen(host)
+            print url.read(1024)
+            self.queue.task_done()
+
 start = time.time()
+def main():
+    for i in range(5):
+        thread = ThreadUrl(queue)
+        thread.setDaemon(True)
+        thread.start()
+        
+        for host in hosts:
+            queue.put(host)
+    queue.join()
 
-for host in hosts:
-    url = urllib2.urlopen(host)
-    print url.read(1024)
-
+main()
 print "Elapsed Time: %s" % (time.time() - start)
