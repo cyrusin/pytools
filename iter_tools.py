c52846af3fb3c4dc5004f8e648e191e23d802ab1
lishuai <li.shuai@youku.com>
2015-04-21 16:19:12 +0800

    continue working on iter_tools

--- iter_tools.py
+++ iter_tools.py
@@ -6,6 +6,7 @@ url: https://docs.python.org/2/library/itertools.html
 """
 
 import itertools
+import operator
 
 def take(n, iterable):
     '''Return first n items of the iterable as a list'''
@@ -14,3 +15,58 @@ def take(n, iterable):
 def tabulate(function, start=0):
     '''Return function(0), function(1), ...'''
     return itertools.imap(function, itertools.count(start))
+
+def nth(iterable, n, default=None):
+    '''Return the nth element of iterable'''
+    return next(itertools.islice(iterable, n, None), default)
+
+def quantify(iterable, pred=bool):
+    '''Count how many times the predicate is True'''
+    return sum(itertools.imap(pred, iterable))
+
+def padnone(iterable):
+    '''Returns the iterable's elements and None indefinitely.'''
+    return itertools.chain(iterable, itertools.repeat(None))
+
+def ncycles(iterable, n):
+    '''Returns the elements of iterable for n times'''
+    return itertools.chain.from_iterable(itertools.repeat(tuple(iterable), n))
+
+def dotproduct(vec1, vec2):
+    return sum(itertools.imap(operator.mul, vec1, vec2))
+
+def flatten(listOfLists):
+    '''Flatten one level of list'''
+    return itertools.chain.from_iterable(listOfLists)
+
+def repeatfunc(function, times=None, *args):
+    '''Repeat calls with specified times and args'''
+    if times is None:
+        return itertools.starmap(function, itertools.repeat(args))
+    return itertools.starmap(function, itertools.repeat(args, times))
+
+def pairwise(iterable):
+    '''s -> (s[0], s[1]), (s[1], s[2]),...'''
+    iter_0, iter_1 = itertools.tee(iterable)
+    next(iter_1, None)
+    return itertools.izip(iter_0, iter_1)
+
+def grouper(iterable, n, fillvalue=None):
+    '''grouper("abcdefg", 3, 'x') -> 'abc', 'edf', "gxx"'''
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
+
+def unique_everseen(iterable, key=None):
+    '''List items unique ever seen, preserving order'''
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for item in itertools.ifilterfalse(seen.__contains__, iterable):
+            seen_add(item)
+            yield item
+    else:
+        for item in iterable:
+            k = key(item)
+            if k not in seen:
+                seen_add(k)
+                yield item
