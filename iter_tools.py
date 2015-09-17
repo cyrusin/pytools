d6114244e44cac9049b079a22117311a7632d82e
lishuai <li.shuai@youku.com>
2015-09-17 16:02:33 +0800

    add Alphabet which can generate some alphabet

--- iter_tools.py
+++ iter_tools.py
@@ -83,3 +83,37 @@ def iter_except(func, exception, first=None):
             yield func()
     except exception:
         pass
+
+
+class Alphabet(object):
+    '''iterable object that give you alphabet with the number you want.
+    Such as:
+        Alphabet(27, cap=True) -> A, B, C, ...Z, A
+        Alphabet(28, cap=True) -> A, B, C, ...Z, A, B
+        Alphabet(28, cap=False) -> a, b, c, ...z, a, b
+        Alphabet(28) -> a, b, c, ...z, a, b
+      
+    '''
+    def __init__(self, n, cap=False):
+        self.stop = n
+        self.value = 0
+        if cap:
+            self.alph = [chr(i) for i in xrange(65, 91)]
+        else:
+            self.alph = [chr(i) for i in xrange(97, 123)]
+        self.alph_length = len(self.alph)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.value == self.alph_length:
+            self.value = 0
+            self.stop = self.stop - self.alph_length
+
+        if self.value >= self.stop:
+            raise StopIteration
+
+        x = self.alph[self.value]
+        self.value = self.value + 1
+        return x
