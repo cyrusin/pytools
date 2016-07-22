1f124466d2ac5b0af7cda561c32003a3020fbfb8
lishuai <lysh0526@gmail.com>
2016-07-22 15:19:05 +0800

    Add main_loop to loop read/write events

--- echo_server_greenlet.py
+++ echo_server_greenlet.py
@@ -5,9 +5,48 @@
 import collections
 import functools
 import socket
+import sys
+from select import select
 import greenlet
 
+if len(sys.argv) != 2:
+    print "Usage: python echo_server_greenlet.py 127.0.0.1:8080"
+
+try:
+    host, port = sys.argv[1].split(':')
+except Exception, e:
+    print e
+    sys.exit(-1)
+
 grn_tasks = collections.deque()
+recv_wait = {}
+send_wait = {}
+
+
+def main_loop():
+    while any([grn_tasks, recv_wait, send_wait]):
+        while not grn_tasks:
+            can_recv, can_send, _ = select(recv_wait, send_wait, [])
+            for s in can_recv:
+                grn_tasks.append(('grn_r', recv_wait.pop(s)))
+            for s in can_send:
+                grn_tasks.append(('grn_r', send_wait.pop(s)))
+
+        g_state, g_task =  grn_tasks.popleft()
+        if g_state == 'grn_r': # greenlet is active, just switch to it
+            try:
+                if g_task.dead:
+                    continue
+                sw_signal, sw_what = g_task.switch()
+                if sw_signal == 'ev_recv':
+                    recv_wait[sw_what] = g_task
+                elif sw_signal == 'ev_send':
+                    send_wait[sw_what] = g_task
+            except Exception:
+                continue
+        elif g_state == 'grn_n': # init task in new greenlet
+            grn_tasks.append(('grn_r', greenlet.greenlet(g_task)))
+
 
 def server(host_address, handle_client):
     '''start a server on host_address
@@ -63,3 +102,10 @@ def handle_client(handle_request, client):
 def echo_handler(req):
     return req + b'\n'
 
+# Init first greenlet for server
+client_handler = functools.partial(handle_client, echo_handler)
+server_task = functools.partial(server, (host, port), client_handler)
+grn_server = greenlet.greenlet(server_task)
+
+grn_tasks.append(grn_server)
+main_loop()
