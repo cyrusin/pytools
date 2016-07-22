c005a12045cb55e238cce140076e6e1d212cb642
lishuai <lysh0526@gmail.com>
2016-07-22 15:01:56 +0800

    Add function to handle client

--- echo_server_greenlet.py
+++ echo_server_greenlet.py
@@ -29,3 +29,37 @@ def server(host_address, handle_client):
         print "Connection", addr
         task = functools.partial(handle_client, client)
         grn_tasks.append(('grn_n', task)) #raw task
+
+
+def handle_client(handle_request, client):
+    '''handle client request using request_handler
+    @client: client socket object
+    '''
+    error = False
+    grn_current = greenlet.getcurrent()
+    grn_parent = grn_current.parent
+    while True:
+        grn_parent.switch(('ev_recv', client))
+        try:
+            req = client.recv(100)
+        except Exception:
+            client.close()
+            break
+        if not req: #client has closed
+            break
+        try:
+            resp = handle_request(req)
+        except Exception:
+            resp = b'Error in server\n'
+            error = True
+        grn_parent.switch(('ev_send', client))
+        client.send(resp)
+        if error:
+            client.close()
+            break
+    print 'closed'
+
+
+def echo_handler(req):
+    return req + b'\n'
+
