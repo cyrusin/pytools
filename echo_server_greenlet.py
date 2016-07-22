d1fcaf39a3de3bcc7c9c2af641eae729b9611ec9
lishuai <lysh0526@gmail.com>
2016-07-22 14:46:47 +0800

    Generate new greenlet for each client

--- echo_server_greenlet.py
+++ echo_server_greenlet.py
@@ -2,8 +2,12 @@
 # -*- coding:utf-8 -*-
 '''Echo server based on greenlet
 '''
+import collections
+import functools
 import socket
 import greenlet
+
+grn_tasks = collections.deque()
 
 def server(host_address, handle_client):
     '''start a server on host_address
@@ -19,4 +23,9 @@ def server(host_address, handle_client):
     sock.listen(5)
     print "server listen on ", host_address
 
-    
+    while True:
+        grn_parent.switch(('ev_recv', sock)) #switch to main greenlet
+        client, addr = sock.accept() #blocking
+        print "Connection", addr
+        task = functools.partial(handle_client, client)
+        grn_tasks.append(('grn_n', task)) #raw task
